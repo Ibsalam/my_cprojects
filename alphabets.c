b6c6bdea296e3e63a11c141dee1e8af71fea7ded
ibsalam <asio4edu@gmail.com>
2023-10-16 17:22:23 +0100

    Print alphabets in capital letters

---
+++ alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Print all alphabets from a to z
+ */
+int main ()
+{
+	char alphabet = 'A';
+	
+	while(alphabet <= 'Z')
+{
+	printf("%c\n", alphabet);
+	alphabet++;
+}
+	return 0;
+}
